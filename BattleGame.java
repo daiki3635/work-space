c53d0eb8f0657932f6313bf875bde237d532f3b1
daiki3635 <daiki363538@yahoo.co.jp>
2020-04-16 16:18:58 +0900

    minor change about Battlegame.java

---
+++ BattleGame.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Battlegame{
+	public static void main(String args[]){
+		new Battlegame().execute();
+	}
+	public void execute(){
+		init();
+	}
+	public void init(){
+		
+	}
+}
