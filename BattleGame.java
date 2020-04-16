fc068dc310b7e84085a74245955b9c01d2a121e9
daiki3635 <daiki363538@yahoo.co.jp>
2020-04-16 16:39:00 +0900

    minor change about Battlegame.java

--- BattleGame.java
+++ BattleGame.java
@@ -1,13 +1,14 @@
 import java.util.*;
+import java.io.*;
 
 class BattleGame{
-	public static void main(String args[]){
+	public static void main(String args[]){ //メインメソッド
 		new BattleGame().execute();
 	}
-	public void execute(){
+	public void execute(){ //実行
 		init();
 	}
-	public void init(){
-
+	public void init(){ //初期化
+		
 	}
 }
